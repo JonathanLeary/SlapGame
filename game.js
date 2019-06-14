323d2a5497ea4571a292fc3cef7039ff4bcdcebb
Jonathan Leary <ollietrickster@gmail.com>
2019-06-14 14:31:04 -0600

    Changed kick to punch

--- game.js
+++ game.js
@@ -1,14 +1,18 @@
 let health = 100;
 
-function punch() {
+function slap() {
   //Reduce health by 5
   health -= 5
 }
-function kick() {
-
+function punch () {
+//Reduce health b10
+health -= 10
+kick()
 }
 function baseballBat() {
-
+//Reduce health by 20
+  health -= 20
+  baseballBat()
 }
 
 function block() {
